9e142484fe7250ceb6598abc51d42937a30a581d
Kim Neunert <k9ert@gmx.de>
2022-02-26 18:09:14 +0100

    progress

--- src/k9ert/specterext/dice/service.py
+++ src/k9ert/specterext/dice/service.py
@@ -1,4 +1,5 @@
 import datetime
+from cryptoadvance.specter.services.callbacks import after_serverpy_init_app, every5seconds
 import json
 import logging
 import pytz
@@ -13,6 +14,8 @@ from cryptoadvance.specter.user import User
 from cryptoadvance.specter.services.service import Service, devstatus_alpha, devstatus_prod
 from cryptoadvance.specter.addresslist import Address
 from cryptoadvance.specter.wallet import Wallet
+from cryptoadvance.specter.devices.bitcoin_core import BitcoinCore
+from cryptoadvance.specter.util.psbt_creator import PsbtCreator
 
 logger = logging.getLogger(__name__)
 
@@ -43,6 +46,21 @@ class DiceService(Service):
     AUTOWITHDRAWAL_ID = "autowithdrawal_id"
     AUTOWITHDRAWAL_THRESHOLD = "withdrawal_threshold"
 
+    def __init__(self, active, specter):
+        self.processed_txs = []
+        super().__init__(active, specter)
+
+    def callback(self, callback_id):
+        if callback_id == every5seconds:
+            try:
+                wallet = self.get_associated_wallet()
+                mytxlist = wallet.txlist()
+                for tx in mytxlist:
+                    self.process_tx(tx, wallet)
+            except Exception as e:
+                logger.exception(e)
+            
+
     @classmethod
     def is_access_token_valid(cls):
         service_data = cls.get_current_user_service_data()
@@ -57,16 +75,17 @@ class DiceService(Service):
     def has_refresh_token(cls):
         return cls.REFRESH_TOKEN in cls.get_current_user_service_data()
 
-    @classmethod
-    def get_associated_wallet(cls) -> Wallet:
-        """Get the Specter `Wallet` that is currently associated with Swan auto-withdrawals"""
-        service_data = cls.get_current_user_service_data()
-        if not service_data or cls.SPECTER_WALLET_ALIAS not in service_data:
+
+    def get_associated_wallet(self) -> Wallet:
+        #service_data = cls.get_current_user_service_data()
+        service_data = self.specter.service_unencrypted_storage_manager.get_current_user_service_data(self.id)
+
+        if not service_data or self.SPECTER_WALLET_ALIAS not in service_data:
             # Service is not initialized; nothing to do
             return
         try:
             return app.specter.wallet_manager.get_by_alias(
-                service_data[cls.SPECTER_WALLET_ALIAS]
+                service_data[self.SPECTER_WALLET_ALIAS]
             )
         except SpecterError as e:
             logger.debug(e)
@@ -74,10 +93,84 @@ class DiceService(Service):
             # TODO: keep ignoring or remove the unknown wallet from service_data?
             return
 
-    @classmethod
-    def set_associated_wallet(cls, wallet: Wallet):
+    def set_associated_wallet(self, wallet: Wallet):
         """Set the Specter `Wallet` that is currently associated with Swan auto-withdrawals"""
-        cls.update_current_user_service_data({cls.SPECTER_WALLET_ALIAS: wallet.alias})
+        #cls.update_current_user_service_data({cls.SPECTER_WALLET_ALIAS: wallet.alias})
+        self.specter.service_unencrypted_storage_manager.set_current_user_service_data(self.id, {self.SPECTER_WALLET_ALIAS: wallet.alias})
+
+    def process_tx(self, tx, wallet: Wallet):
+        
+        txid = tx["txid"]
+        # we only process txs to address 10
+        rel_addr = wallet.get_address(10)
+        #print(f"processing {tx}")
+        if tx["txid"] in self.processed_txs:
+            return
+        if tx["address"] != rel_addr:
+            #print("skipping  "+txid)
+            return
+        
+        
+        if tx["amount"] == 20: # need a better way to sort out mining txs
+            return
+        if txid[0] == "f":
+            print(f"    --> Sorry, Lost!")
+        else:
+            print()
+            
+            winning_address = self.calc_sender_address(txid, wallet)
+            print("    --> Jackpot! This tx won:" + txid)
+            print("    -->              address:" + winning_address)
+            # The device of a Howallet is a bitcoinCorewallet 
+            core: BitcoinCore = wallet.devices[0]
+            print(core.name)
+            try:
+                core.setup_device("", app.specter.wallet_manager)
+            except:
+                pass
+            rpc = app.specter.wallet_manager.rpc.wallet(core.wallet_name)
+            # Doesn't work
+            #rpc.keypoolrefill(4)
+            #rpc.sendtoaddress(winning_address,1)
+            psbt_json = {
+                "recipients" : [
+                    { 
+                        "address": winning_address,
+                        "amount": 0.1,
+                        "unit": "btc",
+                        "label": "someLabel"
+                    },
+                ],
+                "rbf_tx_id": "",
+                "subtract_from": "1",
+                "fee_rate": "64",
+                "rbf": True
+            }
+            psbt = PsbtCreator(app.specter, wallet, "json", request_json=psbt_json).create_psbt(wallet)
+            current_psbt = wallet.pending_psbts[psbt["tx"]["txid"]]
+            b64psbt = str(current_psbt)
+            device = wallet.devices[0]
+            signed_psbt = app.specter.device_manager.get_by_alias(
+                        device.alias
+                    ).sign_psbt(b64psbt, wallet, "")
+            raw = wallet.rpc.finalizepsbt(b64psbt)
+            current_psbt.update(signed_psbt["psbt"], raw)
+            print(current_psbt)
+            wallet.rpc.sendrawtransaction(raw)
+
+            
+        self.processed_txs.append(txid)
+
+    def calc_sender_address(self, txid, wallet):
+        tx = wallet.rpc.decoderawtransaction(wallet.rpc.gettransaction(txid)["hex"])
+        tx_input_raw = wallet.rpc.gettransaction(tx["vin"][0]["txid"])
+        tx_inp = wallet.rpc.decoderawtransaction(tx_input_raw["hex"])
+        winning_addr = tx_inp["vout"][0]["scriptPubKey"]["address"]
+        return winning_addr
+
+
+
+            
 
     @classmethod
     def reserve_addresses(
