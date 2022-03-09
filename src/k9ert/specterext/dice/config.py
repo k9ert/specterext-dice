c7316fc6bca45920d4dcbf38f737f4b747b753fb
Kim Neunert <k9ert@gmx.de>
2022-03-09 13:29:29 +0100

    xtra file for AppProductionConfig

--- src/k9ert/specterext/dice/config.py
+++ src/k9ert/specterext/dice/config.py
@@ -13,11 +13,3 @@ class BaseConfig:
 class ProductionConfig(BaseConfig):
     pass
 
-class AppProductionConfig(SpecterProductionConfig):
-    # Where should the User endup if he hits the root of that domain?
-    ROOT_URL_REDIRECT = "/spc/ext/dice"
-    # I guess this is the only extension which should be available?
-    EXTENSION_LIST = [
-        "k9ert.specterext.dice.service"
-    ]
-    SPECTER_DATA_FOLDER=os.path.expanduser("~/.dice")
